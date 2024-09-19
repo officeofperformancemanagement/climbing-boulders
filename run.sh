bdadaa0ae270b436872d06c456fc361aa2033676
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2024-09-19 16:52:28 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-curl --output data.csv "https://overpass-api.de/api/interpreter?data=nwr%0A%20%20%5Bclimbing%3Dboulder%5D%0A%20%20%2831.876868%2C-93.827638%2C38.194383%2C-77.139406%29%3B%0Aout%20body%20center%3B%0A"
+curl --output data.csv "https://overpass-api.de/api/interpreter?data=%5Bout%3Acsv%28%27name%27%2C%3A%3Atype%2C%3A%3Alat%2C%3A%3Alon%3Btrue%3B%27%2C%27%29%5D%3B%0A%0Anwr%0A%20%20%5Bclimbing%3Dboulder%5D%0A%20%20%2831.876868%2C-93.827638%2C38.194383%2C-77.139406%29%3B%0Aout%20body%20center%3B"
