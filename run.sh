1dd004cb5667c61ab5e3fe931fa5f828b0139dd5
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2024-09-19 16:38:37 -0400

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,1 @@
+curl --output data.csv "https://overpass-api.de/api/interpreter?data=nwr%0A%20%20%5Bclimbing%3Dboulder%5D%0A%20%20%2831.876868%2C-93.827638%2C38.194383%2C-77.139406%29%3B%0Aout%20body%20center%3B%0A"
